8365b54894646abcb2915ed52e977828b83ce781
Rabiya <rabiyaimt786@gmail.com>
2023-04-07 16:22:00 +0500

    added index.html

---
+++ app.js
@@ -0,0 +1,110 @@
+// question no 1
+// let firstNumber=+ prompt("select any no 1:");
+// let secondNumber=+ prompt("select any no 2:");
+
+// if(firstNumber > secondNumber){
+// console.log(firstNumber + "is greater");
+// }
+// else if(secondNumber > firstNumber){
+// console.log(secondNumber + "is greater");
+// }
+// else{
+// console.log("both nos are equal");
+// }
+
+//  question no 2
+// let num= +prompt("enter no:")
+// if(num > 0){
+//   console.log("+ sign");
+// }
+
+// if(num < 0){
+//   console.log("- sign");
+// }
+// if(num === 0){
+//   console.log("entered no is zero");
+// }
+
+// question no 3
+// let no_1 = +prompt("enter no 1:");
+// let no_2 = +prompt("enter no 2:");
+// let no_3 = +prompt("enter no 3:");
+// let no_4 = +prompt("enter no 4:");
+// let no_5 = +prompt("enter no 5:");
+
+// if( no_1 >  no_2 &&  no_1 >  no_3 &&  no_1 >  no_4 &&  no_1 >  no_5){
+//   console.log( no_1 +" is greater");
+// }
+// else if( no_2 >  no_1 &&  no_2 >  no_3 &&  no_2 >  no_4 &&  no_2 >  no_5){
+//   console.log( no_2 +" is greater");
+// }
+// else if( no_3 >  no_1 &&  no_3 >  no_2 &&  no_3 >  no_4 &&  no_3 >  no_5){
+//   console.log( no_3 +" is greater");
+// }
+// else if( no_4 >  no_1 &&  no_4 >  no_2 &&  no_4 >  no_3 &&  no_4 >  no_5){
+//   console.log( no_4 +" is greater");
+// }
+// else if( no_5 >  no_1 &&  no_5 >  no_2 &&  no_5 >  no_3 &&  no_5 >  no_4){
+//   console.log( no_5 +" is greater");
+// }
+// else{
+//   console.log( "no number is greater you have enter two equal numbers");
+// }
+
+// question no 4
+// for(i=0 ; i <=15; i++){
+//   if(i % 2 == 0){
+//     console.log(i + ": is even");
+//   }
+//   else{
+//     console.log(i + ": is odd");
+//   }
+// }
+
+// question no 5
+// let Marks=+prompt("enter your marks");
+// if(Marks<=0){
+//   console.log("invalid marks")
+// }
+// else if(Marks >= 90  ){
+//   console.log( "your grade is A");
+// }
+
+// else if(Marks >=80){
+//   console.log("your grade is B");
+// }
+// else if(Marks>=70){
+//   console.log("your grade is C");
+// }
+// else if(Marks>=60){
+//   console.log("your grade is D");
+// }
+// else {
+//   console.log("your grade is F");
+// }
+
+
+
+// question no 6
+// for (i=1; i<=100; i++){
+//   if(i % 3 == 0 && i % 5== 0){
+//     console.log(i +"FizzBuzz");
+//   }
+//   else if(i % 3 == 0){
+//     console.log(i +"Fizz");
+//   }
+//   else if(i % 5== 0 ){
+//     console.log(i +"Buzz");
+//   }
+// }
+
+// question no 7
+// for(i = 1; i<=5; i++){
+//   for(j = i; j<=5; j++){
+//     document.write("*");
+//   }
+//   document.write("<br>");
+// }
+
+
+
